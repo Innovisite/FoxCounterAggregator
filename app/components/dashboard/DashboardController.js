e6674a9192169904e5bd668609cc673972918457
Ionel POP <i.pop@foxstream.fr>
2017-04-26 17:47:39 +0200

    Add the export function to the dashboard

--- app/components/dashboard/DashboardController.js
+++ app/components/dashboard/DashboardController.js
@@ -2,40 +2,39 @@
  * Manage the dashboard data
  **/
 
-(function(){
+(function() {
 
     require('../services/DashboardParamsService');
-    
+    require('../services/ExportService');
+
     angular.module('FSCounterAggregatorApp').controller('DashboardController', [
-	'$scope',
-	'DashboardParamsService',	
-	function(
-	    $scope,
-	    DashboardParamsService
-	) {	   
-	    $scope.params = DashboardParamsService;
-	    
-	    $scope.params.loadParams().then(function() {
-		$scope.params.loadData();
-	    });
+        '$scope',
+        'DashboardParamsService',
+        'ExportService',
+        function(
+            $scope,
+            DashboardParamsService,
+            ExportService
+        ) {
+            $scope.params = DashboardParamsService;
 
-	    $scope.switchTimeZone = function() {
-		$scope.params.useTimeZone = !$scope.params.useTimeZone;
-		$scope.params.reloadData();
-	    };
-	    
-	    /*
-	    $scope.$watch('params.period', function(newPeriod, oldPeriod) {
-		if(newPeriod !== oldPeriod) {
-		    $scope.params.loadData();
-		}
-	    });
-	    */
+            $scope.params.loadParams().then(function() {
+                $scope.params.loadData();
+            });
 
-	    $scope.exportPrint = function() {
-		window.print();
-	    };
+            $scope.switchTimeZone = function() {
+                $scope.params.useTimeZone = !$scope.params.useTimeZone;
+                $scope.params.reloadData();
+            };
 
-	}]);
+            $scope.exportCurrentData = function() {
+                var dateFormat = "YYYY-MM-DD";
+                var fileName = "RawData_" + $scope.params.period.startDate.format(dateFormat) +
+                    "__" + $scope.params.period.endDate.format(dateFormat) + ".csv";
+                var csvData = ExportService.ConvertDataToCSV($scope.params.data, $scope.params.sites);
+                ExportService.ProposeToDownload(csvData, fileName, 'text/csv;encoding:utf-8');
+            };
+        }
+    ]);
 
 }());

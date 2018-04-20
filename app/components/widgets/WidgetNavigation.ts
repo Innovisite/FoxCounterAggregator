5ebdc109e7aabbcf4e6d8f11667242a2aeb658e0
fabien <fabien.pelisson@innovisite.com>
2018-04-20 09:11:03 +0200

    Add breadcrumbs in navigation widget

--- app/components/widgets/WidgetNavigation.ts
+++ app/components/widgets/WidgetNavigation.ts
@@ -5,7 +5,7 @@ import { SiteItem, SiteNavItem } from "../types/site";
 angular.module('FSCounterAggregatorApp').
     directive('fcaWidgetNavigation', function () {
         return {
-            scope: {                
+            scope: {
                 three: '=',
                 threePos: '=',
                 onNextSite: '&'
@@ -15,36 +15,46 @@ angular.module('FSCounterAggregatorApp').
                 function (
                     $scope: any
                 ) {                    
-                                    
-                    $scope.navPos = this.threePos;                                                    
 
-                    $scope.$watch("threePos", function(newPos: SiteNavItem, oldPos: SiteNavItem) {
+                    $scope.breadCrumbs = [];
+                    $scope.navPos = this.threePos;
 
-                        if(newPos !== undefined) {
+                    $scope.$watch("threePos", function (newPos: SiteNavItem, oldPos: SiteNavItem) {
+
+                        if (newPos !== undefined) {
                             $scope.initNav();
+                            $scope.updateBreadCrumbs();
                         }
 
                     });
 
-                    $scope.navParent = function($event: Event) {
-                        $event.stopPropagation();                        
-                        if($scope.navPos.parent) {
+                    $scope.navParent = function ($event: Event) {
+                        $event.stopPropagation();
+                        if ($scope.navPos.parent) {
                             $scope.navPos = $scope.navPos.parent;
                         }
                     };
 
-                    $scope.navChild = function($event: Event, child: SiteNavItem) {
-                        $event.stopPropagation();                        
+                    $scope.navChild = function ($event: Event, child: SiteNavItem) {
+                        $event.stopPropagation();
                         $scope.navPos = child;
                     };
 
-                    $scope.initNav = function() {
+                    $scope.initNav = function () {
                         $scope.navPos = $scope.threePos;
                     };
 
-                    $scope.goNav = function(item: SiteNavItem) {                        
-                        $scope.onNextSite({$event: item});
-                    };                    
+                    $scope.goNav = function (item: SiteNavItem) {
+                        $scope.onNextSite({ $event: item });
+                    };
+
+                    $scope.updateBreadCrumbs = function () {
+                        const breadCrumbs: SiteNavItem[] = [];
+                        for (let p = $scope.threePos; p !== undefined; p = p.parent) {
+                            breadCrumbs.push(p);
+                        }
+                        $scope.breadCrumbs = breadCrumbs.reverse();
+                    };
 
                 }],
             templateUrl: "build/html/WidgetNavigationView.html"

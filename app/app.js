e49f7a6b0f04551ca476da11844cca2bdf93c71e
Ionel POP <i.pop@foxstream.fr>
2018-08-17 17:31:32 +0200

    Suppression de la page Dashboard, utilisation de la page generic à la place

--- app/app.js
+++ app/app.js
@@ -1,187 +1,187 @@
 /**
-* @namespace FSCounterAggregatorApp
-*/
+ * @namespace FSCounterAggregatorApp
+ */
 
-(function () {
+(function() {
 
-  // module main declaration
-  //require('angular');
-  require('angular-ui-codemirror');
-  require('./components/modules/ngReallyClickModule');
-  require('./components/services/LayoutService');
+    // module main declaration
+    //require('angular');
+    require('angular-ui-codemirror');
+    require('./components/modules/ngReallyClickModule');
+    require('./components/services/LayoutService');
 
-  angular.module('FSCounterAggregatorApp', [
-    'ui.router',
-    'ngResource',
-    'ui.bootstrap',
-    'daterangepicker',
-    'nvd3',
-    'datatables',
-    'datatables.bootstrap',
-    'ui.codemirror',
-    'adminLTE',
-    'ngReallyClickModule',
-    'angularScreenfull',
-    'angularjs-gauge'
-  ]);
+    angular.module('FSCounterAggregatorApp', [
+        'ui.router',
+        'ngResource',
+        'ui.bootstrap',
+        'daterangepicker',
+        'nvd3',
+        'datatables',
+        'datatables.bootstrap',
+        'ui.codemirror',
+        'adminLTE',
+        'ngReallyClickModule',
+        'angularScreenfull',
+        'angularjs-gauge'
+    ]);
 
-  // controllers
-  require('./components/dashboard/DashboardController');
-  require('./components/monitoring/MonitoringController');
-  require('./components/settings/CurrentUser');
-  require('./components/settings/SettingsSiteItems');
-  require('./components/settings/SettingsSiteMembers');
-  require('./components/settings/SettingsUsers');
-  require('./components/settings/SettingsSites');
-  require('./components/settings/SettingsPerSite');
-  require('./components/settings/SettingsUsersSites');
-  require('./components/settings/SettingsPerUser');
+    // controllers
+    require('./components/dashboard/DashboardController');
+    require('./components/monitoring/MonitoringController');
+    require('./components/settings/CurrentUser');
+    require('./components/settings/SettingsSiteItems');
+    require('./components/settings/SettingsSiteMembers');
+    require('./components/settings/SettingsUsers');
+    require('./components/settings/SettingsSites');
+    require('./components/settings/SettingsPerSite');
+    require('./components/settings/SettingsUsersSites');
+    require('./components/settings/SettingsPerUser');
 
-  // directives
-  require('./components/dashboard/SideMenu');
-  require('./components/topbar/TopBar');
-  require('./components/settings/MemberEditor');
-  require('./components/settings/SiteEditor');
-  require('./components/settings/UserEditor');
-  require('./components/settings/UserSiteEditor');
-  require('./components/widgets/CalendarPicker');
-  require('./components/widgets/GraphKPI');
-  require('./components/widgets/heatmap/HeatMapKPI');
-  require('./components/widgets/StatBox');
-  require('./components/widgets/StatBoxKPI');
-  require('./components/widgets/GaugeBoxKPI');
-  require('./components/widgets/TableKPI');
-  require('./components/widgets/UserDashboard');
-  require('./components/widgets/WidgetNavigation');
+    // directives
+    require('./components/dashboard/SideMenu');
+    require('./components/topbar/TopBar');
+    require('./components/settings/MemberEditor');
+    require('./components/settings/SiteEditor');
+    require('./components/settings/UserEditor');
+    require('./components/settings/UserSiteEditor');
+    require('./components/widgets/CalendarPicker');
+    require('./components/widgets/GraphKPI');
+    require('./components/widgets/heatmap/HeatMapKPI');
+    require('./components/widgets/StatBox');
+    require('./components/widgets/StatBoxKPI');
+    require('./components/widgets/GaugeBoxKPI');
+    require('./components/widgets/TableKPI');
+    require('./components/widgets/UserDashboard');
+    require('./components/widgets/WidgetNavigation');
 
-  // kpis
-  angular.module('FSCounterAggregatorApp')
-    .controller('KPISumGeneric', require('./components/kpis/KPISumGeneric').KPISumGeneric)
-    .controller('KPISumMax', require('./components/kpis/KPISumMax').KPISumMax)    
-    .controller('KPIPeriodGeneric', require('./components/kpis/KPIPeriodGeneric').KPIPeriodGeneric)
-    .controller('KPISitesPeriod', require('./components/kpis/KPISitesPeriod').KPISitesPeriod)
-    .controller('KPITypicalDay', require('./components/kpis/KPITypicalDay').KPITypicalDay)
-    .controller('KPIServerGeneric', require('./components/kpis/KPIServerGeneric').KPIServerGeneric)
-    .controller('KPIMax', require('./components/kpis/KPIMax'))
-    .controller('KPISum', require('./components/kpis/KPISum'))
-    .controller('KPIMean', require('./components/kpis/KPIMean'))
-    .controller('KPIMaxSiteRatio', require('./components/kpis/KPIMaxSiteRatio'))
-    .controller('KPIMaxPeriod', require('./components/kpis/KPIMaxPeriod'))
-    .controller('SiteNavigationController', require('./components/widgets/SiteNavigationController'))
-    .controller('CurrentSiteController', require('./components/widgets/CurrentSiteController'))
-    .controller('LiveModeController', require('./components/widgets/LiveModeController'))
-    .controller('DashboardController', require('./components/dashboard/DashboardController'));
+    // kpis
+    angular.module('FSCounterAggregatorApp')
+        .controller('KPISumGeneric', require('./components/kpis/KPISumGeneric').KPISumGeneric)
+        .controller('KPISumMax', require('./components/kpis/KPISumMax').KPISumMax)
+        .controller('KPIPeriodGeneric', require('./components/kpis/KPIPeriodGeneric').KPIPeriodGeneric)
+        .controller('KPISitesPeriod', require('./components/kpis/KPISitesPeriod').KPISitesPeriod)
+        .controller('KPITypicalDay', require('./components/kpis/KPITypicalDay').KPITypicalDay)
+        .controller('KPIServerGeneric', require('./components/kpis/KPIServerGeneric').KPIServerGeneric)
+        .controller('KPIMax', require('./components/kpis/KPIMax'))
+        .controller('KPISum', require('./components/kpis/KPISum'))
+        .controller('KPIMean', require('./components/kpis/KPIMean'))
+        .controller('KPIMaxSiteRatio', require('./components/kpis/KPIMaxSiteRatio'))
+        .controller('KPIMaxPeriod', require('./components/kpis/KPIMaxPeriod'))
+        .controller('SiteNavigationController', require('./components/widgets/SiteNavigationController'))
+        .controller('CurrentSiteController', require('./components/widgets/CurrentSiteController'))
+        .controller('LiveModeController', require('./components/widgets/LiveModeController'))
+        .controller('DashboardController', require('./components/dashboard/DashboardController'));
 
-  // filters
-  require('./components/pipes/HourFormatPipe');
-  require('./components/pipes/SiteNamePipe');  
+    // filters
+    require('./components/pipes/HourFormatPipe');
+    require('./components/pipes/SiteNamePipe');
 
-  // services
-  angular.module('FSCounterAggregatorApp')
-    .service('UserService', require('./components/services/UserService').UserService)    
-    .service('DashboardParamsService', require('./components/services/DashboardParamsService').DashboardParamsService)
-    .service('WidgetStyleService', require('./components/services/WidgetStyleService').WidgetStyleService)
-    .service('DataService', require('./components/services/DataService').DataService)
-    .service('ExportService', require('./components/services/ExportService').ExportService);
+    // services
+    angular.module('FSCounterAggregatorApp')
+        .service('UserService', require('./components/services/UserService').UserService)
+        .service('DashboardParamsService', require('./components/services/DashboardParamsService').DashboardParamsService)
+        .service('WidgetStyleService', require('./components/services/WidgetStyleService').WidgetStyleService)
+        .service('DataService', require('./components/services/DataService').DataService)
+        .service('ExportService', require('./components/services/ExportService').ExportService);
 
-  // Configure routes
-  angular.module('FSCounterAggregatorApp').config(['$urlRouterProvider', '$stateProvider',
-    function ($urlRouterProvider, $stateProvider) {
+    // Configure routes
+    angular.module('FSCounterAggregatorApp').config(['$urlRouterProvider', '$stateProvider',
+        function($urlRouterProvider, $stateProvider) {
 
-      // For any unmatched url, redirect to main page
-      $urlRouterProvider.otherwise("/dashboard");
+            // For any unmatched url, redirect to main page
+            $urlRouterProvider.otherwise("/generic");
 
-      $stateProvider.
-        state('dashboard', {
-          url: '/dashboard', 
-          templateUrl: 'build/html/DashboardView.html',
-          controller: 'DashboardController',
-          pageName: 'Counters / Sites Overview',
-          category: 'Counters'
-        }).
-        state('generic', {
-          url: '/generic', 
-          templateUrl: 'build/html/GenericView.html',
-          controller: 'DashboardController',
-          pageName: 'Counters / Generic',
-          category: 'Counters'
-        }).
-        state('mydashboard', {
-          url: '/mydashboard', 
-          templateUrl: 'build/html/MyDashboardView.html',
-          controller: 'DashboardController',
-          pageName: 'Counters / MyDashboard',
-          category: 'Counters'
-        }).
-        state('monitoring', {
-          url: '/monitoring', 
-          templateUrl: 'build/html/MonitoringView.html',
-          controller: 'MonitoringController',
-          pageName: 'Monitoring / Sites Overview',
-          category: 'Monitoring'
-        }).
-        state('current_user', {
-          url: '/current_user', 
-          templateUrl: 'build/html/CurrentUserView.html',
-          pageName: "My account",
-          controller: 'CurrentUser',
-          category: "Settings"
-        }).
-        state('settings_site_items', {
-          url: '/settings_site_items',
-          templateUrl: 'build/html/SettingsSiteItemsView.html',
-          controller: 'SettingsSiteItems',
-          pageName: "Site cameras",
-          category: "Settings"
-        }).
-        state('settings_site_members', {
-          url: '/settings_site_members', 
-          templateUrl: 'build/html/SettingsSiteMembersView.html',
-          controller: 'SettingsSiteMembers',
-          pageName: "Site Members",
-          category: "Settings"
-        }).
-        state('settings_site_members.id', {
-          url: ':siteId'
-        }).
-        state('settings_users', {
-          url: '/settings_users', 
-          templateUrl: 'build/html/SettingsUsersView.html',
-          controller: 'SettingsUsers',
-          pageName: "Users management",
-          category: "Settings"
-        }).
-        state('settings_sites', {
-          url: '/settings_sites', 
-          templateUrl: 'build/html/SettingsSitesView.html',
-          controller: 'SettingsSites',
-          pageName: "Sites - Users management",
-          category: "Settings"
-        }).
-        state('settings_per_site', {
-          url: '/settings_per_site', 
-          templateUrl: 'build/html/SettingsPerSiteView.html',
-          controller: 'SettingsPerSite',
-          pageName: "Per Site management",
-          category: "Settings"
-        }).
-        state('settings_per_site.id', {
-          url: ':siteId'
-        }).
-        state('settings_users_sites', {
-          url: '/settings_users_sites', 
-          templateUrl: 'build/html/SettingsUsersSitesView.html',
-          controller: 'SettingsUsersSites',
-          pageName: "Users - Sites management",
-          category: "Settings"
-        }).
-        state('settings_per_user', {
-          url: '/settings_per_user/:userId?', 
-          templateUrl: 'build/html/SettingsPerUserView.html',
-          controller: 'SettingsPerUser',
-          pageName: "Per User management",
-          category: "Settings"
-        });
-    }
-  ]);
+            $stateProvider.
+            state('dashboard', {
+                url: '/dashboard',
+                templateUrl: 'build/html/DashboardView.html',
+                controller: 'DashboardController',
+                pageName: 'Counters / Sites Overview',
+                category: 'Counters'
+            }).
+            state('generic', {
+                url: '/generic',
+                templateUrl: 'build/html/GenericView.html',
+                controller: 'DashboardController',
+                pageName: 'Counters / Generic',
+                category: 'Counters'
+            }).
+            state('mydashboard', {
+                url: '/mydashboard',
+                templateUrl: 'build/html/MyDashboardView.html',
+                controller: 'DashboardController',
+                pageName: 'Counters / MyDashboard',
+                category: 'Counters'
+            }).
+            state('monitoring', {
+                url: '/monitoring',
+                templateUrl: 'build/html/MonitoringView.html',
+                controller: 'MonitoringController',
+                pageName: 'Monitoring / Sites Overview',
+                category: 'Monitoring'
+            }).
+            state('current_user', {
+                url: '/current_user',
+                templateUrl: 'build/html/CurrentUserView.html',
+                pageName: "My account",
+                controller: 'CurrentUser',
+                category: "Settings"
+            }).
+            state('settings_site_items', {
+                url: '/settings_site_items',
+                templateUrl: 'build/html/SettingsSiteItemsView.html',
+                controller: 'SettingsSiteItems',
+                pageName: "Site cameras",
+                category: "Settings"
+            }).
+            state('settings_site_members', {
+                url: '/settings_site_members',
+                templateUrl: 'build/html/SettingsSiteMembersView.html',
+                controller: 'SettingsSiteMembers',
+                pageName: "Site Members",
+                category: "Settings"
+            }).
+            state('settings_site_members.id', {
+                url: ':siteId'
+            }).
+            state('settings_users', {
+                url: '/settings_users',
+                templateUrl: 'build/html/SettingsUsersView.html',
+                controller: 'SettingsUsers',
+                pageName: "Users management",
+                category: "Settings"
+            }).
+            state('settings_sites', {
+                url: '/settings_sites',
+                templateUrl: 'build/html/SettingsSitesView.html',
+                controller: 'SettingsSites',
+                pageName: "Sites - Users management",
+                category: "Settings"
+            }).
+            state('settings_per_site', {
+                url: '/settings_per_site',
+                templateUrl: 'build/html/SettingsPerSiteView.html',
+                controller: 'SettingsPerSite',
+                pageName: "Per Site management",
+                category: "Settings"
+            }).
+            state('settings_per_site.id', {
+                url: ':siteId'
+            }).
+            state('settings_users_sites', {
+                url: '/settings_users_sites',
+                templateUrl: 'build/html/SettingsUsersSitesView.html',
+                controller: 'SettingsUsersSites',
+                pageName: "Users - Sites management",
+                category: "Settings"
+            }).
+            state('settings_per_user', {
+                url: '/settings_per_user/:userId?',
+                templateUrl: 'build/html/SettingsPerUserView.html',
+                controller: 'SettingsPerUser',
+                pageName: "Per User management",
+                category: "Settings"
+            });
+        }
+    ]);
 }());
